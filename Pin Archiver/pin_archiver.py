af5caf44d535dd19ba2fcb13f33b28560b231f52
Xaad <Saad888@users.noreply.github.com>
2019-05-20 03:56:09 -0600

    5/20/2019

---
+++ Pin Archiver/pin_archiver.py
@@ -0,0 +1,30 @@
+import discord
+
+with open("token.txt", "r") as file:
+    TOKEN = file.read()
+
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await message.channel.send(msg)
+
+    if message.content.startswith('!exit'):
+        await message.channel.send("Fine I'll leave.")
+        await client.logout()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(TOKEN)
